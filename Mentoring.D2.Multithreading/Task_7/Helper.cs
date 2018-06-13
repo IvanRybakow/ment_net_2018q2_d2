ea16aca35d5dd5bad6e9a4b2b52e325d1623a9ed
Ivan Rybakov <ivan_rybakov1@epam.com>
2018-06-13 18:42:55 +0300

    Added 7th task

---
+++ Mentoring.D2.Multithreading/Task_7/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    public static class Helper
+    {
+        private static List<Task> tasks = new List<Task>();
+        private static readonly Action<Task> onlyIfNotRanToCompletion = task => Console.WriteLine($"I will be printed if previous task not ran to completion");
+        private static readonly Action<Task> anyway = task => Console.WriteLine($"I will be printed anyway");
+        private static readonly Action<Task> afterFailInTheSamethread = task =>
+            Console.WriteLine($"I will be printed if previous task failed. On the same thread {Thread.CurrentThread.ManagedThreadId}");
+        private static readonly Action<Task> afterCancelOutsideThreadPool = task =>
+        Console.WriteLine($"I will be printed if previous task canceled. Outside of thread pool. Thread From Pool: {Thread.CurrentThread.IsThreadPoolThread}");
+        public static void AddContinuations(Task t)
+        {
+            t.ContinueWith(anyway);
+            t.ContinueWith(onlyIfNotRanToCompletion, TaskContinuationOptions.NotOnRanToCompletion);
+            t.ContinueWith(afterFailInTheSamethread, TaskContinuationOptions.OnlyOnFaulted | TaskContinuationOptions.ExecuteSynchronously);
+            t.ContinueWith(afterCancelOutsideThreadPool, CancellationToken.None, TaskContinuationOptions.OnlyOnCanceled, new CustomTaskScheduler());
+        }
+    }
+}
---
+++ Mentoring.D2.Multithreading/Task_7/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/////////Successfully ended task\\\\\\\\\\");
+            var tcs1 = new TaskCompletionSource<bool>();
+            tcs1.SetResult(true);
+            Helper.AddContinuations(tcs1.Task);
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+
+            Console.WriteLine("/////////Canceled task\\\\\\\\\\");
+            var tcs2 = new TaskCompletionSource<bool>();
+            tcs2.SetCanceled();
+            Helper.AddContinuations(tcs2.Task);
+            Thread.Sleep(TimeSpan.FromSeconds(0.5));
+
+            Console.WriteLine("/////////Failed task\\\\\\\\\\");
+            var t = Task.Factory.StartNew(() => {
+                Console.WriteLine($"failed on thread: {Thread.CurrentThread.ManagedThreadId}");
+                throw new Exception();
+            });
+            Helper.AddContinuations(t);
+            Console.ReadKey();
+        }
+    }
+}
