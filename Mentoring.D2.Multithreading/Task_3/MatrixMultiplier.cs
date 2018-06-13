aa367841de2aeb3b5eee21fec44f0f7cae873a85
Ivan Rybakov <ivan_rybakov1@epam.com>
2018-06-13 15:48:59 +0300

    Added tasks 1-6 multithreading

---
+++ Mentoring.D2.Multithreading/Task_3/MatrixMultiplier.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class MatrixMultiplier
+    {
+        public int[][] Multiply(int[][] matrixA, int[][] matrixB)
+        {
+            int rowsA = matrixA.Length;
+            int rowsB = matrixB.Length;
+            int colsA = matrixA[0].Length;
+            int colsB = matrixB[0].Length;
+            if (colsA != rowsB)
+            {
+                throw new ArgumentException("Theese matrices can not be multiplied");
+            }
+            int[][] result = MatrixUtils.CreateMatrix(rowsA, colsB, true);
+            Parallel.For(0, rowsA, i =>
+            {
+                Parallel.For(0, colsB, j =>
+                {
+                    int[] col = matrixB.Select(row => row[j]).ToArray();
+                    result[i][j] = VectorProduct(matrixA[i], col);
+                });
+            });
+            return result;
+        }
+
+
+        private int VectorProduct(int[] row, int[] col)
+        {
+            int result = 0;
+            for (int i = 0; i < row.Length; i++)
+            {
+                result += row[i] * col[i];
+            }
+
+            return result;
+        }
+    }
+    
+}
---
+++ Mentoring.D2.Multithreading/Task_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_4
+{
+    class Program
+    {
+        private static int _start;
+        static void Main(string[] args)
+        {
+            bool validInput = false;
+            do
+            {
+                Console.WriteLine("Please enter a valid number:");
+                var input = Console.ReadLine();
+                validInput = int.TryParse(input, out _start);
+            } while (!validInput);
+
+            var t = new Thread(Countdown);
+            t.Start(_start);
+            t.Join();
+            Console.WriteLine("Finish");
+            Console.ReadKey();
+        }
+
+
+        private static void Countdown(object counter)
+        {
+            var c = (int)counter;
+            Console.WriteLine($"Thread #{Thread.CurrentThread.ManagedThreadId}. Counter: {--c}");
+            if (_start - c == 10)
+            {
+                return;
+            }
+            var t = new Thread(Countdown);
+            t.Start(c);
+            t.Join();
+        }
+    }
+}
