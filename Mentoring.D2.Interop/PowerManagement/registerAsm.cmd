f5a53e0d8fd8c093180c7ee1fcb58a9791d47dd0
Ivan Rybakov <ivan_rybakov1@epam.com>
2018-07-03 14:29:30 +0300

    small fix

--- Mentoring.D2.Interop/PowerManagement/registerAsm.cmd
+++ Mentoring.D2.Interop/PowerManagement/registerAsm.cmd
@@ -1,1 +1,1 @@
-﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe PowerManagement.dll /tlb /codebase
+﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe bin\debug\PowerManagement.dll /tlb /codebase
--- Mentoring.D2.Interop/PowerManagement/unregisterAsm.cmd
+++ Mentoring.D2.Interop/PowerManagement/unregisterAsm.cmd
@@ -1,1 +1,1 @@
-﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe PowerManagement.dll /unregister
+﻿C:\Windows\Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe bin\debug\PowerManagement.dll /unregister
