6437527a514acdb6a268652166780818f2f6ba55
Ivan Rybakov <ivan_rybakov1@epam.com>
2018-08-01 11:50:56 +0300

    Added debugging tusk

---
+++ Mentoring.D2.Debugging/Mentoring.D2.Debugging/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mentoring.D2.Debugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var netInterface = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
+                .FirstOrDefault();
+            if (netInterface == null)
+            {
+                Console.WriteLine("empty");
+                Console.ReadKey();
+                return;
+            }
+            var bytes1 = netInterface.GetPhysicalAddress().GetAddressBytes();
+            var bytes2 = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+            var key = bytes1.Select((item, index) => (item ^ bytes2[index]) * 10);
+            Console.WriteLine(string.Join("-", key.Select(item => item.ToString())));
+            Console.ReadKey();
+        }
+    }
+}
